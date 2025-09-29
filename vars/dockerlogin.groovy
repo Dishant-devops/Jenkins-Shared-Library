b15b4dd0bf94c8cf1cd883ab9b0fd5bd3db5163b
Dishant-devops <dishantsaini203@gmail.com>
2025-09-29 17:11:44 +0530

    Update dockerlogin.groovy

--- vars/dockerlogin.groovy
+++ vars/dockerlogin.groovy
@@ -1,5 +1,9 @@
-def call (String credentialsid , String repo , String tag) {
-     withCredentials([usernamePassword(credentialsId: "${DOCKER_CREDENTIAL_ID}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                        sh "sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
-                        sh "sudo docker push ${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${IMAGE_TAG}"
+def call (String credentialsid , String dockerhub_username , String image_name, String image_tag ) {
+     withCredentials([usernamePassword
+     (credentialsId: "${DOCKER_CREDENTIAL_ID}", 
+      usernameVariable: 'DOCKER_USERNAME',
+      passwordVariable: 'DOCKER_PASSWORD')]) {
+      sh "sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
+      sh "sudo docker push ${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${IMAGE_TAG}"
+}
 }
